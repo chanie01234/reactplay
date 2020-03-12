8f8c96e4eab9e09b6bcd6fb7d4e1894ebd10d04c
Chantelle Levy <chanie01234@users.noreply.github.com>
2020-03-12 14:04:19 -0400

    wrote express backend

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const ENV = process.env.NODE_ENV;
+const PORT = 8080;
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = app; 
